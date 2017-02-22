2a28128b9f7a9ba47ddf154ff49e5a55ad8d765b
Jamie Quinn <jjquinn42@gmail.com>
2017-02-22 18:20:07 +0000

    Very simple solution atm

--- facts_generator.js
+++ facts_generator.js
@@ -1,1 +1,13 @@
-document.write("I heard Ross has javascript.");
+var url = "facts.txt";
+var fact_element = document.getElementById("fact");
+var facts;
+$.get( url, function( data ) {
+  facts = data.split("\n");
+});
+
+document.getElementById("moustache").onclick = displayFact;
+
+function displayFact() {
+  var rand_index = Math.floor(Math.random() * (facts.length-1)); 
+  fact_element.innerHTML = facts[rand_index];
+}
